c0e08153fd62ba5abfeccc6cb7dd8cb6417a779f
BillyQuispe <carlos.quispe.v@tecsup.edu.pe>
2024-11-21 09:34:03 -0500

    sdsa

--- setup.sh
+++ setup.sh
@@ -1,38 +1,10 @@
 #!/bin/bash
 
-# Variables
-NETWORK_NAME="mi_red"
-STACK_NAME="voting-app"
-COMPOSE_FILE="docker-compose.yml"
+# Clonar el repositorio
+git clone https://github.com/BillyQuispe/EXAMEN-DAEA
 
-# Inicializar Docker Swarm si no está ya inicializado
-if [ $(docker info --format '{{.Swarm.LocalNodeState}}') != "active" ]; then
-    echo "Inicializando Docker Swarm..."
-    docker swarm init
-else
-    echo "Docker Swarm ya está inicializado."
-fi
+# Cambiar al directorio del proyecto
+cd EXAMEN-DAEA
 
-# Crear la red Docker si no existe
-echo "Creando la red Docker si no existe..."
-docker network ls | grep -q $NETWORK_NAME || docker network create $NETWORK_NAME
-
-# Verificar si el archivo docker-compose.yml existe
-if [ ! -f "$COMPOSE_FILE" ]; then
-    echo "El archivo docker-compose.yml no se encuentra. Asegúrate de tenerlo en el directorio correcto."
-    exit 1
-fi
-
-# Desplegar la pila (stack) usando Docker Compose
-echo "Desplegando la pila en Docker Swarm..."
-docker stack deploy -c $COMPOSE_FILE $STACK_NAME
-
-# Escalar servicios según sea necesario (por ejemplo, Flask a 3 réplicas)
-echo "Escalando el servicio Flask..."
-docker service scale ${STACK_NAME}_flask=3
-
-# Comprobamos el estado de los servicios
-echo "Comprobando el estado de los servicios..."
-docker service ls
-
-echo "¡La configuración y el despliegue en Docker Swarm se han completado con éxito!"
+# Construir y levantar los contenedores Docker
+docker-compose up --build -d
